3a3965b988d4b438048400eaee014c374d767c99
xAEvi <105865102+xAEvi@users.noreply.github.com>
2023-11-21 10:25:38 -0500

    Added CRUD Interface

--- ProyectoPOE/Controller/ControllerCliente.cs
+++ ProyectoPOE/Controller/ControllerCliente.cs
@@ -1,21 +1,65 @@
 ﻿using frmMain.Model;
+using ProyectoPOE.Controller;
+using System;
 
 namespace frmMain.Controller
 {
-    internal class ControllerCliente
+    internal class ControllerCliente : ICRUDController<Cliente>
     {
         private List<Cliente> ListaCliente;
         public ControllerCliente()
         {
             ListaCliente = new List<Cliente>();
         }
-        public List<Cliente> GetCliente()
+
+        public void Crear(Cliente dato)
         {
-            return ListaCliente;
+            ListaCliente.Add(dato);
+            MessageBox.Show("Cliente agregado con éxito.");
         }
-        public void AgregarCliente(Cliente cliente)
+
+        public Cliente Leer(string cedula)
         {
-            ListaCliente.Add(cliente);
+            Cliente clienteEncontrado = ListaCliente.Find(c => c.Cedula == cedula);
+
+            if (clienteEncontrado != null)
+            {
+                return clienteEncontrado;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Actualizar(string cedula, Cliente nuevoDato)
+        {
+            Cliente clienteEncontrado = Leer(cedula);
+
+            if (clienteEncontrado != null)
+            {
+                clienteEncontrado = nuevoDato;
+                MessageBox.Show("Cliente actualizado con éxito.");
+            } 
+            else
+            {
+                MessageBox.Show("Cliente no encontrado.");
+            }
+        }
+
+        public void Eliminar(string cedula)
+        {
+            int indice = ListaCliente.FindIndex(cliente => cliente.Cedula == cedula);
+
+            if (indice != -1)
+            {
+                ListaCliente.RemoveAt(indice);
+                MessageBox.Show("Cliente ha sido eliminado.");
+            }
+            else
+            {
+                MessageBox.Show("Cliente no encontrado.");
+            }
         }
     }
 }
---
+++ ProyectoPOE/Controller/ICRUDController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPOE.Controller
+{
+    public interface ICRUDController<T>
+    {
+        void Crear(T dato);
+        T Leer(string indice);
+        void Actualizar(string indice, T dato);
+        void Eliminar(string indice);
+    }
+}
